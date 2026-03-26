9bb80958235bb5796708dfe44994711cc4c88a4a
João Marcello <marcello.jao1611@gmail.com>
2026-03-26 12:59:29 -0300

    Traits scraper

--- main.py
+++ main.py
@@ -1,26 +1,10 @@
-from json import load
-from os import path
-from selenium import webdriver
-
+from scrapers.traits_scraper import TraitsScraper
 base_url = "https://2e.aonprd.com"
 
 
-async def get_feats(url: str):
-    pass
-
-
 def main():
-    options = webdriver.FirefoxOptions()
-    options.add_argument("-headless")
-    driver = webdriver.Firefox(options=options)
-
-    # if not path.isfile("categories.json"):
-    #     get_categories_json(base_url=base_url)
-
-    json_file = open("categories.json")
-    categories = load(json_file)
-
-    driver.quit()
+    traits = TraitsScraper()
+    traits.scrape('https://2e.aonprd.com/Traits.aspx')
 
 
 if __name__ == "__main__":
---
+++ scrapers/traits_scraper.py
@@ -0,0 +1,46 @@
+from .base_scraper import BaseScraper
+from bs4 import BeautifulSoup
+
+
+class TraitsScraper(BaseScraper):
+    def parse(self, soup: BeautifulSoup) -> dict:
+        result = {"traits": []}
+        main = soup.find(id='main')
+        traits = main.contents[9]
+        traits_types = traits.find_all(name="h2", class_="title")
+
+        for traits_type in traits_types:
+            current = traits_type
+            while current is not None and current.name != ('br'):
+                if current.name is None or (current.string == traits_type.string):
+                    current = current.next_sibling
+                    continue
+
+                description = self.get_trait_description(current.a['href'])
+                trait = {
+                    "name": current.string,
+                    "type": traits_type.string,
+                    "desc": description
+                }
+                current = current.next_sibling
+
+                result["traits"].append(trait)
+
+                print(result)
+
+    def get_trait_description(self, url: str) -> str:
+        base_url = "https://2e.aonprd.com"
+        html = self.fetcher.fetch(url=base_url + '/' + url)
+        soup = BeautifulSoup(html, 'html.parser')
+        description_section = soup.find(
+            class_='external-link').next_sibling.next_sibling
+        description = ''
+
+        while description_section.name != 'br':
+            if description_section.string is None:
+                description += description_section
+            else:
+                description += description_section.string
+            description_section = description_section.next_sibling
+
+        return description

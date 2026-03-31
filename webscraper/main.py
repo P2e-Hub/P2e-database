c3b6bdc370ccbe232c11e9ffad22d3bba6b521e9
João Marcello <marcello.jao1611@gmail.com>
2026-03-31 18:40:06 -0300

    Postgres connection

--- webscraper/main.py
+++ webscraper/main.py
@@ -1,10 +1,15 @@
 from scrapers.traits_scraper import TraitsScraper
+from sqlalchemy import create_engine, text
 base_url = "https://2e.aonprd.com"
 
 
 def main():
-    traits = TraitsScraper()
-    traits.scrape('https://2e.aonprd.com/Traits.aspx')
+    engine = create_engine("postgresql+psycopg://postgres:example@100.75.243.92:5432/pathfinder-2e")
+    with engine.connect() as conn:
+        conn.execute(text("CREATE TABLE TRAITS (x int, y int)"))
+        conn.commit()
+    # traits = TraitsScraper()
+    # traits.scrape('https://2e.aonprd.com/Traits.aspx')
 
 
 if __name__ == "__main__":
